16a3ad9dd4bd4cac70ceb974613fd6a87b8e857c
Timofey <tvershinin375@gmail.com>
2023-07-06 14:10:27 +0300

    Добавьте файлы проекта.

---
+++ racketa/Header.h
@@ -0,0 +1,71 @@
+#pragma once
+#include "windows.h"
+#include <iostream>
+#include <string>
+#include <conio.h>
+#include <math.h>
+#include <thread>
+
+const int mapH = 30;
+const int  mapW = 120;
+char map[mapH][mapW + 1];
+
+
+
+typedef struct
+{
+	int x;
+	int y;
+	int width;
+} TRacket;
+
+typedef struct
+{
+	int x;
+	int Sy;
+	int Ly;
+	int LxMin;
+	int ex;
+	int Lex;
+	int Swidth;
+	int Lwidth;
+	int Smallheight;
+	int Largeheight;
+	int hp;
+} TBall;
+
+typedef struct
+{
+	int x1;
+	int y1;
+	float dx1;
+	float dy1;
+	int x2;
+	int y2;
+	float dx2;
+	float dy2;
+	float speed;
+	float alpha;
+} TBullet;
+
+TRacket racket;
+TBall ball;
+TBullet bullet;
+bool fire = false;
+
+
+
+
+void setcur(int x, int y);
+void ininRacket();
+void putRacket();
+void racketMove(int x, int y);
+void initMap();
+void showMap();
+void initbullet();
+void putbullet();
+void firebullet();
+void movebullet(float x, float y);
+void initBall(); 
+void putSmallBall();
+void putLargeBall();
---
+++ racketa/racketa.cpp
@@ -0,0 +1,353 @@
+﻿
+#include "Header.h"
+
+
+void setcur(int x, int y)
+{
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+void hit()
+{
+	if (map[bullet.y1][bullet.x1] == 'Q' || map[bullet.y1][bullet.x1] == 'o')
+	{
+		ball.hp -= 10;
+		fire = false;
+	}
+	if (map[bullet.y2 + 1][bullet.x2] == 'Q' || map[bullet.y2][bullet.x2] == 'o')
+	{
+		ball.hp -= 10;
+		fire = false;
+
+	}
+}
+
+void moveball()
+{
+	ball.Ly += 1;
+	//Sleep(100);
+	if (ball.Ly - 10 >= mapH || ball.hp <= 0)
+		initBall();
+}
+
+void initBall()
+{
+	ball.x = rand() % mapW - 20;
+	
+	
+	
+	ball.Sy = 0;
+	ball.Ly = 0;
+	ball.Swidth = 10;
+	ball.Lwidth = 24;
+	ball.ex = ball.x + ball.Swidth;
+	ball.Lex = ball.x + ball.Lwidth;
+	ball.Smallheight = 6;
+	ball.Largeheight = 11;
+	ball.hp = 10;
+	ball.LxMin = ball.x - 7;
+
+	while (ball.LxMin < 0)
+	{
+		ball.LxMin = ball.x - 7;
+		ball.x += 1;
+	}
+	
+}
+
+void putSmallBall()
+{
+	for (int i = ball.x; i < ball.ex; i++)
+	{
+		map[ball.Sy][i] = 'o';
+	}
+	for (int i = ball.x+2; i < ball.ex-2; i++)
+	{
+		map[ball.Sy+2][i] = 'Q';
+	}
+	for (int i = ball.x + 1; i < ball.ex - 1; i++)
+	{
+		map[ball.Sy + 1][i] = 'o';
+	}
+	for (int i = ball.x + 2; i < ball.ex - 2; i++)
+	{
+		map[ball.Sy - 1][i] = 'o';
+	}
+	for (int i = ball.x + 4; i < ball.ex - 4; i++)
+	{
+		map[ball.Sy + 3][i] = 'Q';
+	}
+	for (int i = ball.x + 4; i < ball.ex - 4; i++)
+	{
+		map[ball.Sy - 2][i] = 'o';
+	}
+}
+
+
+void putLargeBall()
+{
+	
+
+	if (ball.Ly >= 0 && ball.Ly < mapH)
+	{
+		for (int i = ball.x + 1; i < ball.ex - 1; i++)
+		{
+			map[ball.Ly][i] = 'Q';
+		}
+	}
+	if (ball.Ly >= 1 && ball.Ly < mapH + 1)
+	{
+		for (int i = ball.x - 2; i < ball.ex + 2; i++)
+		{
+			map[ball.Ly - 1][i] = 'Q';
+		}
+	}
+	if (ball.Ly >= 2 && ball.Ly < mapH + 2)
+	{
+		for (int i = ball.x - 5; i < ball.ex + 5; i++)
+		{
+			map[ball.Ly - 2][i] = '1';
+		}
+	}
+	if (ball.Ly >= 3 && ball.Ly < mapH + 3)
+	{
+		for (int i = ball.x - 6; i < ball.ex + 6; i++)
+		{
+			map[ball.Ly - 3][i] = 'o';
+		}
+	}
+	if (ball.Ly >= 4 && ball.Ly < mapH + 4)
+	{
+		for (int i = ball.x - 7; i < ball.ex + 7; i++)
+		{
+			map[ball.Ly - 4][i] = 'o';
+		}
+	}
+	if (ball.Ly >= 5 && ball.Ly < mapH + 5)
+	{
+		for (int i = ball.x - 7; i < ball.ex + 7; i++)
+		{
+			map[ball.Ly - 5][i] = 'o';
+		}
+	}
+	if (ball.Ly >= 6 && ball.Ly < mapH + 6)
+	{
+		for (int i = ball.x - 6; i < ball.ex + 6; i++)
+		{
+			map[ball.Ly - 6][i] = 'o';
+		}
+	}
+	if (ball.Ly >= 7 && ball.Ly < mapH + 7)
+	{
+		for (int i = ball.x - 5; i < ball.ex + 5; i++)
+		{
+			map[ball.Ly - 7][i] = 'o';
+		}
+	}
+	if (ball.Ly >= 8 && ball.Ly < mapH + 8)
+	{
+		for (int i = ball.x - 2; i < ball.ex + 2; i++)
+		{
+			map[ball.Ly - 8][i] = 'o';
+		}
+	}
+	if (ball.Ly >= 9 && ball.Ly < mapH + 9)
+	{
+		for (int i = ball.x + 1; i < ball.ex - 1; i++)
+		{
+			map[ball.Ly - 9][i] = 'o';
+		}
+	}
+	if (ball.Ly >= 10 && ball.Ly < mapH + 10)
+	{
+		for (int i = ball.x + 4; i < ball.ex - 4; i++)
+		{
+			map[ball.Ly - 10][i] = 'o';
+		}
+	}
+}
+
+
+void movebullet(float x, float y)
+{
+	if (bullet.y1 >= 0)
+	{
+		bullet.dx1 = x + 2;
+		bullet.dy1 = y - 2;
+		bullet.dx2 = x + racket.width - 2;
+		bullet.dy2 = y - 2;
+
+		bullet.x1 = (int)round(bullet.dx1);
+		bullet.y1 = (int)round(bullet.dy1);
+		bullet.x2 = (int)round(bullet.dx2);
+		bullet.y2 = (int)round(bullet.dy2);
+	}
+	else
+	{
+		if (bullet.y1 < 0)
+		{
+			bullet.y1 += 5;
+			bullet.y2 += 5;
+		}
+		fire = false;
+
+		//movebullet(racket.x, racket.y);
+	}
+}
+
+void firebullet()
+{
+	movebullet(bullet.x1 + cos(bullet.alpha) * bullet.speed - 1.6, bullet.y1 + sin(bullet.alpha) * bullet.speed);
+}
+
+void putbullet()
+{
+	if (bullet.x1 == bullet.dx1)
+	{
+		map[bullet.y1][bullet.x1] = ' ';
+		map[bullet.y2][bullet.x2] = ' ';
+	}
+	else
+	{
+		map[bullet.y1 + 2][bullet.x1] = '*';
+		map[bullet.y2 + 2][bullet.x2] = '*';
+	}
+
+}
+
+void initbullet()
+{
+	movebullet(2, 2);
+	bullet.speed = 0, 6;
+	bullet.alpha = 1;
+}
+
+void ininRacket()
+{
+
+	racket.width = 9;
+	racket.y = mapH - 1;
+	racket.x = (mapW / 2) - racket.width;
+
+}
+
+void putRacket()
+{
+	for (int i = racket.x + 1; i < racket.x + racket.width; i++)
+	{
+		map[racket.y][i] = '_';
+		map[racket.y - 1][i] = '_';
+	}
+	map[racket.y][racket.x] = '|';
+	map[racket.y][racket.x + racket.width] = '|';
+
+	map[racket.y][racket.x + 4] = '|';
+	map[racket.y][racket.x + racket.width - 4] = '|';
+
+	map[racket.y - 1][racket.x + 4] = '|';
+	map[racket.y - 1][racket.x + racket.width - 4] = '|';
+
+	map[racket.y - 2][racket.x + 4] = '/';
+	map[racket.y - 2][racket.x + racket.width - 4] = '\\';
+
+	map[racket.y - 1][racket.x + 2] = '|';
+	map[racket.y - 1][racket.x + racket.width - 2] = '|';
+
+
+}
+
+void racketMove(int x, int y)
+{
+	racket.x = x;
+	racket.y = y;
+
+
+
+	//Sleep(5);
+	if (racket.x < 1)
+		racket.x = racket.x + 1;
+	if (racket.x + racket.width > mapW - 1)
+		racket.x = racket.x - 1;
+	if (racket.y < 1)
+		racket.y += 1;
+	if (racket.y > mapH - 1)
+		racket.y -= 1;
+}
+
+void initMap()
+{
+
+	for (int i = 1; i < mapW; i++)
+		map[0][i] = ' ';
+
+	map[0][0] = '|';
+	map[0][mapW - 1] = '|';
+	map[0][mapW] = '\0';
+
+	for (int i = 0; i < mapH; i++)
+		sprintf_s(map[i], map[0]);
+	map[mapH - 1][mapW] = '\0';
+
+}
+
+void showMap()
+{
+
+	for (int i = 0; i < mapH; i++)
+	{
+		printf_s(map[i]);
+	}
+}
+
+
+
+
+int main()
+{
+	char c;
+	srand(time(NULL));
+	ininRacket();
+	initbullet();
+	initBall();
+std::thread th;
+	
+	do
+	{
+		
+		setcur(0, 0);
+
+		initMap();
+		putLargeBall();
+		putbullet();
+		if (fire)
+			firebullet();
+		if(!fire)
+			movebullet(racket.x, racket.y);
+		putRacket();
+		showMap();
+		moveball();
+		hit();
+
+		
+
+		if (GetKeyState('A') < 0)
+			racketMove(racket.x - 1, racket.y);
+		if (GetKeyState('D') < 0)
+			racketMove(racket.x + 1, racket.y);
+		if (GetKeyState('W') < 0)
+			racketMove(racket.x, racket.y - 1);
+		if (GetKeyState('S') < 0)
+			racketMove(racket.x, racket.y + 1);
+		if (GetKeyState('E') < 0)
+			fire = true;
+		Sleep(10);
+		
+		
+	} while (GetKeyState(VK_ESCAPE) >= 0);
+
+	
+	return 0;
+}
